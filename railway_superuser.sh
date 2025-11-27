d912e4d61298333d924e65bbf725b1f499570aea
slendersx65 <slendersx65@gmail.com>
2025-11-27 16:42:36 -0500

    Proyecto Django listo para produccion

--- railway_superuser.sh
+++ railway_superuser.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "=== Creando superusuario en Railway ==="
-
 python tesis_simulador/create_superuser.py
-
 echo "=== Superusuario listo ==="
+
--- tesis_simulador/create_superuser.py
+++ tesis_simulador/create_superuser.py
@@ -1,29 +1,24 @@
 import os
-import sys
 import django
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-
-# --- CONFIGURAR DJANGO MANUALMENTE ---
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tesis_simulador.settings")
 django.setup()
 
-# Ahora sí se pueden importar modelos
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 USERNAME = "Mrfox"
-EMAIL = "Admin@example.com"
+EMAIL = "admin@example.com"
 PASSWORD = r"7tk3\9)9q>RT"
 
 if not User.objects.filter(username=USERNAME).exists():
+    print(">>> Creando superusuario en Railway...")
     User.objects.create_superuser(
         username=USERNAME,
         email=EMAIL,
-        password=PASSWORD
+        password=PASSWORD,
+        full_name="Administrador"
     )
-    print("✔ Superusuario creado correctamente.")
+    print(">>> Superusuario creado.")
 else:
     print("✔ El superusuario ya existe. No se volvió a crear.")
