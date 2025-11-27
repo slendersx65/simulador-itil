ad17090d35df0a498797c66c2a1e169c44bb2f9f
slendersx65 <slendersx65@gmail.com>
2025-11-27 15:05:59 -0500

    Proyecto Django listo para produccion

--- create_superuser.py
+++ create_superuser.py
@@ -3,15 +3,21 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 USERNAME = "Mrfox"
-EMAIL = "Admin@example.com"
-PASSWORD = r"7tk3\9)9q>RT"   # r"" para evitar errores por caracteres especiales
+EMAIL = "admin@example.com"
+PASSWORD = r"7tk3\9)9q>RT"
 
 if not User.objects.filter(username=USERNAME).exists():
-    User.objects.create_superuser(
+    user = User.objects.create_superuser(
         username=USERNAME,
         email=EMAIL,
         password=PASSWORD
     )
-    print("✔ Superusuario creado correctamente.")
+
+    # ESTO ES NECESARIO PORQUE TU MODELO LO USA
+    user.rol = "admin"
+    user.is_admin = True
+    user.save()
+
+    print("✔ Superusuario creado correctamente con rol=admin.")
 else:
     print("✔ El superusuario ya existe. No se volvió a crear.")
