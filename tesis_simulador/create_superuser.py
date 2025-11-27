da94ac3e1aef3931b2ee744064073a1d6f10b9e1
slendersx65 <slendersx65@gmail.com>
2025-11-27 15:57:29 -0500

    Proyecto Django listo para produccion

--- tesis_simulador/create_superuser.py
+++ tesis_simulador/create_superuser.py
@@ -1,5 +1,9 @@
 import os
+import sys
 import django
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 # --- CONFIGURAR DJANGO MANUALMENTE ---
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tesis_simulador.settings")
