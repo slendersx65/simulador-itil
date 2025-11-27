141a8eb1214bc19caadcdc7f2cf95cff7ef9fd82
slendersx65 <slendersx65@gmail.com>
2025-11-27 16:52:12 -0500

    Proyecto Django listo para produccion

--- tesis_simulador/settings.py
+++ tesis_simulador/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0kcb_i!c@d$_(43au42x3bn(@t)05xrxf&&k$fno96g%25nhdr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
