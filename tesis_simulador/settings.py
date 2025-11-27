2b1a26a6c4b8b24712f85368ee96796886e35587
slendersx65 <slendersx65@gmail.com>
2025-11-27 12:10:40 -0500

    Proyecto Django listo para produccion

--- tesis_simulador/settings.py
+++ tesis_simulador/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,7 +32,7 @@ DEBUG = False
 
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['simulador-ITIL.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -84,18 +86,18 @@ WSGI_APPLICATION = 'tesis_simulador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'simulador_db',
-        'USER': 'simulador_user',
-        'PASSWORD': '7tk3\9)9q>RT',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        #'NAME': BASE_DIR / 'db.sqlite3',
+#        'NAME': 'simulador_db',
+#        'USER': 'simulador_user',
+#        'PASSWORD': '7tk3\9)9q>RT',
+#        'HOST': 'localhost',
+#       'PORT': '5432',
+#
+#    }
+#}
 
 
 #DATABASES = {
@@ -109,6 +111,13 @@ DATABASES = {
 #    }
 #}
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -141,13 +150,13 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
