d9b40c96ee1bc484efd4c7dc00b789109c48c5ce
slendersx65 <slendersx65@gmail.com>
2025-11-27 19:16:15 -0500

    Proyecto Django listo para produccion

--- tesis_simulador/settings.py
+++ tesis_simulador/settings.py
@@ -147,7 +147,7 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
@@ -161,5 +161,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
